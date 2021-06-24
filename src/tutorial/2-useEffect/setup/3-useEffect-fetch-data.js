ebd2658e5dee7be5db702a420278c9cb46b2b8dc
MarianoP26 <84840897+MarianoP26@users.noreply.github.com>
2021-06-24 13:54:01 +0200

    useEffect learnings

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,39 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+
+  /*Have in mind useEffect calls getUsers and getUsers modifies the state
+  recalling useEffect again and making it an infinite loop.
+  Thats why useEffect function has an empty array as a dependency*/
+  const getUsers = async() =>{
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+  }
+
+  useEffect(()=>{
+    getUsers();
+  },[]);
+
+  return (
+    <>
+      <h3>Users</h3>
+        <ul className="users">
+          {users.map((user)=>{
+            const { id, login, avatar_url, html_url} = user;
+            return <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          })}
+        </ul>
+    </>  
+  );
 };
 
 export default UseEffectFetchData;
